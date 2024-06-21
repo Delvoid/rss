4661d5463111ab8969d41373d22833df9c9e1045
Delvoid <delvoid.dev@gmail.com>
2024-06-22 00:51:38 +0100

    fix health response

--- api/healthHandler.go
+++ api/healthHandler.go
@@ -5,7 +5,5 @@ import (
 )
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	RespondWithJSON(w, map[string]string{"status": "ok"}, http.StatusOK)
 }
